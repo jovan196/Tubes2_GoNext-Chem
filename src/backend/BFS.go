c1787c93d007e371f58317f1eef3d88eddf8dd58
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-05-07 22:30:33 +0700

    feat: add multiple recipe

--- src/backend/BFS.go
+++ src/backend/BFS.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 func BFS(target string) ([][2]string, bool) {
 	type Node struct {
 		Element string
@@ -40,4 +42,94 @@ func BFS(target string) ([][2]string, bool) {
 	}
 
 	return nil, false
+}
+
+func MultiBFS(target string, max int) [][][2]string {
+	type Node struct {
+		Element string
+		Path    [][2]string
+	}
+
+	type Result struct {
+		Path [][2]string
+	}
+
+	var results [][][2]string
+	resultChan := make(chan [][2]string, max)
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	visitedGlobal := map[string]bool{
+		"Air": true, "Water": true, "Earth": true, "Fire": true,
+	}
+
+	for product, recipes := range Graph {
+		if product == target {
+			for _, pair := range recipes {
+				wg.Add(1)
+				go func(pair [2]string) {
+					defer wg.Done()
+
+					queue := []Node{
+						{Element: pair[0], Path: [][2]string{}},
+						{Element: pair[1], Path: [][2]string{}},
+					}
+
+					visited := map[string]bool{}
+					for k, v := range visitedGlobal {
+						visited[k] = v
+					}
+
+					visited[pair[0]] = true
+					visited[pair[1]] = true
+
+					for len(queue) > 0 {
+						curr := queue[0]
+						queue = queue[1:]
+
+						for prod, recs := range Graph {
+							for _, rec := range recs {
+								if (curr.Element == rec[0] && visited[rec[1]]) || (curr.Element == rec[1] && visited[rec[0]]) {
+									if prod == target {
+										result := append(curr.Path, rec)
+										mu.Lock()
+										if len(results) < max {
+											results = append(results, result)
+											resultChan <- result
+										}
+										mu.Unlock()
+										return
+									}
+									if !visited[prod] {
+										visited[prod] = true
+										queue = append(queue, Node{
+											Element: prod,
+											Path:    append(curr.Path, rec),
+										})
+									}
+								}
+							}
+						}
+					}
+				}(pair)
+			}
+		}
+	}
+
+	// Tunggu semua goroutine selesai
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	// Kumpulkan hasil dari channel
+	finalResults := [][][2]string{}
+	for path := range resultChan {
+		finalResults = append(finalResults, path)
+		if len(finalResults) >= max {
+			break
+		}
+	}
+
+	return finalResults
 }
