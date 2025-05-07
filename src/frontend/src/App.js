e370b55c2f0445830392a5f54da445d78651501e
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-05-07 08:44:37 +0700

    init frontend

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">Tailwind Sudah Aktif 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
