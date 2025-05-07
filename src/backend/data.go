d3a75af16d3170bcba84d718a6d02d649b539008
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-05-07 14:02:37 +0700

    feat: add data

---
+++ src/backend/data.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+var Graph map[string][][2]string
+
+func LoadGraph(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&Graph)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Loaded %d elements from %s\n", len(Graph), filename)
+	return nil
+}
